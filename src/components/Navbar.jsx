64c874a5100c425973e32bdf689449cb07f39b8d
mauung-sudo <69837230+mauung-sudo@users.noreply.github.com>
2022-05-25 21:21:41 +0630

    Add user name in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { useStateProvider } from "../utils/StateProvider";
 
+const Container = styled.div``;
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
-}
+  const [{ userInfo }] = useStateProvider();
+  
+  return (
+    <Container>
+      <div className="search_bar">
+        <FaSearch />
+        <input type="text" placeholder="Artists, songs, or podcast"></input>
+      </div>
+      <div className="avatar">
+        <CgProfile />
+        <span>{userInfo?.userName}</span>
+      </div>
+    </Container>
+  );
+};
 
 export default Navbar;
